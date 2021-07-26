797dc4291648bc0ac73e1efaecca622af8fb48b8
Michelle Oakley <michellecoder38@gmail.com>
2021-07-25 17:53:07 -0700

    addded dynamic PORT for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 // Sets up the Express App
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -14,28 +14,27 @@ app.use(express.json());
 
 // Star Wars Characters (DATA)
 
-const characters = [
-  {
-    routeName: 'yoda',
-    name: 'Yoda',
-    role: 'Jedi Master',
-    age: 900,
-    forcePoints: 2000,
-  },
-  {
-    routeName: 'darthmaul',
-    name: 'Darth Maul',
-    role: 'Sith Lord',
-    age: 200,
-    forcePoints: 1200,
-  },
-  {
-    routeName: 'obiwankenobi',
-    name: 'Obi Wan Kenobi',
-    role: 'Jedi Master',
-    age: 55,
-    forcePoints: 1350,
-  },
+const characters = [{
+        routeName: 'yoda',
+        name: 'Yoda',
+        role: 'Jedi Master',
+        age: 900,
+        forcePoints: 2000,
+    },
+    {
+        routeName: 'darthmaul',
+        name: 'Darth Maul',
+        role: 'Sith Lord',
+        age: 200,
+        forcePoints: 1200,
+    },
+    {
+        routeName: 'obiwankenobi',
+        name: 'Obi Wan Kenobi',
+        role: 'Jedi Master',
+        age: 55,
+        forcePoints: 1350,
+    },
 ];
 
 // Routes
@@ -50,36 +49,36 @@ app.get('/api/characters', (req, res) => res.json(characters));
 
 // Displays a single character, or returns false
 app.get('/api/characters/:character', (req, res) => {
-  const chosen = req.params.character;
+    const chosen = req.params.character;
 
-  console.log(chosen);
+    console.log(chosen);
 
-  /* Check each character routeName and see if the same as "chosen"
-   If the statement is true, send the character back as JSON,
-   otherwise tell the user no character was found */
+    /* Check each character routeName and see if the same as "chosen"
+     If the statement is true, send the character back as JSON,
+     otherwise tell the user no character was found */
 
-  for (let i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
+    for (let i = 0; i < characters.length; i++) {
+        if (chosen === characters[i].routeName) {
+            return res.json(characters[i]);
+        }
     }
-  }
 
-  return res.json(false);
+    return res.json(false);
 });
 
 // Create New Characters - takes in JSON input
 app.post('/api/characters', (req, res) => {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  const newCharacter = req.body;
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    const newCharacter = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
-  console.log(newCharacter);
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
+    console.log(newCharacter);
 
-  characters.push(newCharacter);
-  res.json(newCharacter);
+    characters.push(newCharacter);
+    res.json(newCharacter);
 });
 
 // Starts the server to begin listening
